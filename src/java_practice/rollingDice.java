17b9d6a0745d81c46561f179d71f1fb83425eeae
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-10-29 16:01:35 -0500

    built simple random number generator to refresh myself on java syntax

--- src/java_practice/rollingDice.java
+++ src/java_practice/rollingDice.java
@@ -1,9 +1,15 @@
 package java_practice;
 
+import java.util.Random;
+
 public class rollingDice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Random randomNumber = new Random();
+		int x = randomNumber.nextInt(6) + 1;
+		
+		System.out.println("You rolled a: " + x);
 
 	}
 
