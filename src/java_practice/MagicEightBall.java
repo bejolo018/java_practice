9cf18a77d3a8eefbb14ea12a7974ca5193aa021f
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-10-29 20:31:37 -0500

    Used logic from rolling dice to create something a little more fun!

---
+++ src/java_practice/MagicEightBall.java
@@ -0,0 +1,33 @@
+package java_practice;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MagicEightBall {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Enter your question.");
+		
+		//Scanner allows us to let user type into console
+		Scanner scan = new Scanner(System.in);
+		scan.next();
+		
+		Random randomNumber = new Random();
+		int r = randomNumber.nextInt(5);
+		
+		if(r == 0) {
+			System.out.println("As I see it, yes.");
+		} else if(r == 1) {
+			System.out.println("As I see it, yes.");
+		} else if(r == 2) {
+			System.out.println("Reply hazy, try again.");
+		} else if(r == 3) {
+			System.out.println("Don't count on it.");
+		} else if(r == 4) {
+			System.out.println("Outlook not so good.");
+		}
+		
+	}
+
+}
--- src/java_practice/rollingDice.java
+++ src/java_practice/rollingDice.java
@@ -6,6 +6,7 @@ public class rollingDice {
 
 	public static void main(String[] args) {
 		
+		// Random allows us to pick something at random
 		Random randomNumber = new Random();
 		int x = randomNumber.nextInt(6) + 1;
 		
